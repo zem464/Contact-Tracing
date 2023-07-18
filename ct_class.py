ef47a1ca0879dc2a5df8db70198bdfb3b66a740b
Iris Membrere <irismembrere@gmail.com>
2023-07-19 01:04:16 +0800

    Adding the function for adding information in th csv file

--- ct_class.py
+++ ct_class.py
@@ -6,4 +6,8 @@ class contact_tracing:
     def __init__(self) -> None:
         pass
 
-# Create the functions for appending and reading the csv file
+    # Create the functions for appending and reading the csv file
+    def sub(self, e1, e2, e3, e4):
+        with open("program.csv", "a") as myFile:
+            data = [e1, e2, e3, e4]
+            myFile.write(data)
