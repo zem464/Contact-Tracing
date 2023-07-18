e0b037a3204e13bd3ee3a6725fffa86f33ca99a8
Iris Membrere <irismembrere@gmail.com>
2023-07-19 00:20:31 +0800

    Pseudocode

---
+++ ct_GUI.py
@@ -0,0 +1,13 @@
+# Import Tkinter
+
+# Assign the objects of Tkinter
+
+# Ask for necessary entry like name, age, email, contact number, and the vaccination status
+
+# Create the submit button
+
+# Create the add button
+
+# Create the search button
+
+# Close the program
---
+++ ct_class.py
@@ -0,0 +1,5 @@
+# import csv
+
+# Create class that will be used for the gui
+
+# Create the functions for appending and reading the csv file
