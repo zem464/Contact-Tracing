00170d433d6f17f993fd3e9c0f8dd713990a9993
Iris Membrere <irismembrere@gmail.com>
2023-07-19 00:54:43 +0800

    Creating a class and initializing it

--- ct_class.py
+++ ct_class.py
@@ -1,5 +1,9 @@
 # import csv
+import csv
 
 # Create class that will be used for the gui
+class contact_tracing:
+    def __init__(self) -> None:
+        pass
 
 # Create the functions for appending and reading the csv file
