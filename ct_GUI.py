3321e5820bf34d2ea700beab8ff26c2230887aa5
Iris Membrere <irismembrere@gmail.com>
2023-07-21 02:15:14 +0800

    Re-doing the gui format and making the other file for the program

--- ct_GUI.py
+++ ct_GUI.py
@@ -1,66 +1,61 @@
 # Import Tkinter
-import tkinter
+import tkinter as ct
 from tkinter import *
-from ct_class import contact_tracing
 import csv
 
-# Assigning the instances
-trace = contact_tracing()
+# Organize the functions in the gui using a class
+class contact_tracingGUI(ct.Tk):
+    def __init__(self):
+        super().__init__()
+        # Adding the title and size of the GUI
+        self.title("Contact Tracing")
+        self.geometry("600x400")
 
-# Assign the objects of Tkinter
-ct = tkinter.Tk()
-# Adding the title and size of the GUI
-ct.title("Contact Tracing")
-ct.geometry("600x400")
+        # Ask for necessary entry like name, age, email, contact number, and the vaccination status
+        name = Label(self, text = "Name: ")
+        name.place(x = 50, y = 50)
+        self.e1 = ct.Entry(self, width = 50)
+        self.e1.place(x = 50, y = 80)
 
-# Ask for necessary entry like name, age, email, contact number, and the vaccination status
-name = Label(ct, text = "Name: ")
-name.place(x = 50, y = 50)
-e1 = Entry(ct, width = 50)
-e1.place(x = 50, y = 80)
+        age = Label(self, text = "Age: ")
+        age.place(x = 400, y = 50)
+        self.e2 = Entry(self, width = 25)
+        self.e2.place(x = 400, y = 80)
 
-age = Label(ct, text = "Age: ")
-age.place(x = 400, y = 50)
-e2 = Entry(ct, width = 25)
-e2.place(x = 400, y = 80)
+        email = Label(self, text = "Email: ")
+        email.place(x = 50, y = 120)
+        self.e3 = Entry(self, width = 50)
+        self.e3.place(x = 50, y = 150)
 
-email = Label(ct, text = "Email: ")
-email.place(x = 50, y = 120)
-e3 = Entry(ct, width = 50)
-e3.place(x = 50, y = 150)
+        number = Label(self, text = "Contact number: ")
+        number.place(x = 400, y = 120)
+        self.e4 = Entry(self, width = 25)
+        self.e4.place(x = 400, y = 150)
 
-number = Label(ct, text = "Contact number: ")
-number.place(x = 400, y = 120)
-e4 = Entry(ct, width = 25)
-e4.place(x = 400, y = 150)
+        health = Label(self, text = "About health:")
+        health.place(x = 50, y = 200)
 
-health = Label(ct, text = "About health:")
-health.place(x = 50, y = 200)
+        vac = ct.Label(self, text = "Have you been vaccinated?")
+        vac.place(x = 50, y = 225)
+        self.vac1 = Checkbutton(self, text = "Yes, first dose")
+        self.vac1.place(x = 60, y = 250)
+        self.vac2 = Checkbutton(self, text = "Yes, second dose")
+        self.vac2.place(x = 60, y = 275)
+        self.vac3 = Checkbutton(self, text = "Yes, booster")
+        self.vac3.place(x = 60, y = 300)
+        self.vac4 = Checkbutton(self, text = "Not yet")
+        self.vac4.place(x = 60, y = 325)
 
-vac = Label(ct, text = "Have you been vaccinated?")
-vac.place(x = 50, y = 225)
-vac1 = Checkbutton(ct, text = "Yes, first dose")
-vac1.place(x = 60, y = 250)
-vac2 = Checkbutton(ct, text = "Yes, second dose")
-vac2.place(x = 60, y = 275)
-vac3 = Checkbutton(ct, text = "Yes, booster")
-vac3.place(x = 60, y = 300)
-vac4 = Checkbutton(ct, text = "Not yet")
-vac4.place(x = 60, y = 325)
+        # Create the submit button
+        submitting = Button(self, text = "Submit", command = self.sub)
+        submitting.place(x = 280, y = 235)
 
-def sub():
-    with open("ct_file.csv", "a") as myFile:
-        data = ([e1.get(),  e2.get(), e3.get(), e4.get()])
-        write_this = csv.writer(myFile)
-        write_this.writerow(data)
+    def sub(self):
+        with open("ct_file.csv", "a") as myFile:
+            data = ([self.e1.get(),  self.e2.get(), self.e3.get(), self.e4.get()])
+            write_this = csv.writer(myFile)
+            write_this.writerow(data)
 
-# Create the submit button
-submitting = Button(ct, text = "Submit", command = sub)
-submitting.place(x = 280, y = 235)
+    # Create the add button
 
-# Create the add button
-
-# Create the search button
-
-# Close the program
-ct.mainloop()
+    # Create the search button
---
+++ ct_program.py
@@ -0,0 +1,10 @@
+# Use this file to close the program
+
+# Import the gui
+from ct_GUI import contact_tracingGUI
+
+# Create the instance
+ct_gui = contact_tracingGUI()
+
+# Close program
+ct_gui.mainloop()
