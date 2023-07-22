4e06478b8f94d8081b51bda002bf17f9f1d4fdc3
Iris Membrere <irismembrere@gmail.com>
2023-07-22 11:31:20 +0800

    Arranging the places of the labels and entries

--- ct_GUI.py
+++ ct_GUI.py
@@ -13,50 +13,48 @@ class contact_tracingGUI(ct.Tk):
 
         # Creating the search bar
         here = Label(self, text = "Search here...")
-        here.place(x = 50, y = 45)
-        self.bar = ct.Entry(self, width = 80)
-        self.bar.place(x = 50, y = 60)
+        here.place(x = 50, y = 20)
+        self.bar = ct.Entry(self, width = 65)
+        self.bar.place(x = 50, y = 40)
 
         # Ask for necessary entry like name, age, email, contact number, and the vaccination status
         name = Label(self, text = "Name: ")
-        name.place(x = 50, y = 50)
+        name.place(x = 50, y = 80)
         self.e1 = ct.Entry(self, width = 50)
-        self.e1.place(x = 50, y = 80)
+        self.e1.place(x = 50, y = 110)
         
         age = Label(self, text = "Age: ")
-        age.place(x = 400, y = 50)
+        age.place(x = 400, y = 80)
         self.e2 = ct.Entry(self, width = 25)
-        self.e2.place(x = 400, y = 80)
+        self.e2.place(x = 400, y = 110)
         
         email = Label(self, text = "Email: ")
-        email.place(x = 50, y = 120)
+        email.place(x = 50, y = 150)
         self.e3 = ct.Entry(self, width = 50)
-        self.e3.place(x = 50, y = 150)
+        self.e3.place(x = 50, y = 180)
         
         number = Label(self, text = "Contact number: ")
-        number.place(x = 400, y = 120)
+        number.place(x = 400, y = 150)
         self.e4 = ct.Entry(self, width = 25)
-        self.e4.place(x = 400, y = 150)
+        self.e4.place(x = 400, y = 180)
         
         health = Label(self, text = "About health:")
-        health.place(x = 50, y = 200)
-
-        rad = IntVar()
+        health.place(x = 50, y = 220)
 
         vac = Label(self, text = "Have you been vaccinated?")
-        vac.place(x = 50, y = 225)
-        self.vac1 = ct.Radiobutton(self, text = "Yes, first dose", Variable = rad, value = "1")
-        self.vac1.place(x = 60, y = 250)
-        self.vac2 = ct.Radiobutton(self, text = "Yes, second dose", Variable = rad, value = "2")
-        self.vac2.place(x = 60, y = 275)
-        self.vac3 = ct.Radiobutton(self, text = "Yes, booster", Variable = rad, value = "3")
-        self.vac3.place(x = 60, y = 300)
-        self.vac4 = ct.Radiobutton(self, text = "Not yet", Variable = rad, value = "4")
-        self.vac4.place(x = 60, y = 325)
+        vac.place(x = 50, y = 250)
+        self.vac1 = ct.Radiobutton(self, text = "Yes, first dose", value = "1")
+        self.vac1.place(x = 60, y = 280)
+        self.vac2 = ct.Radiobutton(self, text = "Yes, second dose", value = "2")
+        self.vac2.place(x = 60, y = 300)
+        self.vac3 = ct.Radiobutton(self, text = "Yes, booster", value = "3")
+        self.vac3.place(x = 60, y = 320)
+        self.vac4 = ct.Radiobutton(self, text = "Not yet", value = "4")
+        self.vac4.place(x = 60, y = 340)
 
         # Create the submit button
         submitting = Button(self, text = "Submit", command = self.sub)
-        submitting.place(x = 280, y = 235)
+        submitting.place(x = 285, y = 350)
 
     # Create function for the submit button
     def sub(self):
